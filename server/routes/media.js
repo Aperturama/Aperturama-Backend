f01666f7c9e636570811d3d13afe330bbe35ea5c
Alex Bozeat <alexboz101@gmail.com>
2022-04-11 23:57:52 -0400

    Get raw media endpoint

--- server/routes/media.js
+++ server/routes/media.js
@@ -8,4 +8,22 @@ router.get('/', async(req, res) => {
 
 })
 
+router.get('/:id(\\d+)/media', async(req, res) => {
+
+	// Get file extension from original filename in database
+	const query = await db.query('SELECT filename FROM aperturama.media WHERE media_id = $1', [req.params['id']])
+
+	if(query.rows.length === 1){
+		const extension = query.rows[0]['filename'].match(/\.[^.]+$/)[0]
+		res.sendFile(process.env['MEDIA_ROOT'] + '/' + req.params['id'] + extension, (err) => {
+			if(err){
+				res.sendStatus(500)
+			}
+		})
+	}else{
+		res.sendStatus(404)
+	}
+
+})
+
 module.exports = router
