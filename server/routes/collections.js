b6d849d60d5635dbc534a6c8f334ae046642c681
Alex Bozeat <alexboz101@gmail.com>
2022-04-17 18:51:39 -0400

    Update collection endpoint

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -21,6 +21,24 @@ router.post('/', async(req, res) => {
 
 });
 
+// PUT /collections/<id> - Update a collection (rename)
+router.put('/:id(\\d+)', async(req, res) => {
+
+	// Check if user has access to collection
+	const query = await db.query('SELECT owner_user_id FROM aperturama.collection WHERE collection_id = $1', [req.params['id']]);
+	if(query.rows.length === 1 && query.rows[0]['owner_user_id'] === req.user.sub){
+
+		// Update collection name
+		await db.query('UPDATE aperturama.collection SET name = $1 WHERE collection_id = $2', [req.query['name'], req.params['id']]);
+
+		res.sendStatus(200);
+
+	}else{
+		res.sendStatus(401);
+	}
+
+});
+
 // POST /collections/<id> - Add media to collection
 router.post('/:id(\\d+)', async(req, res) => {
 
