ad225c62195a82ae5f9089759244f4067b8e8337
Alex Bozeat <alexboz101@gmail.com>
2022-04-20 11:51:04 -0400

    Catch collection database errors

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -7,85 +7,97 @@ const crypto = require('crypto');
 // GET /collections - Retrieve list of user's collections
 router.get('/', async(req, res) => {
 
-	const query = await db.query('SELECT collection_id, name FROM aperturama.collection WHERE owner_user_id = $1', [req.user.sub]);
-	// TODO: Error handling
-
-	res.json(query.rows);
+	try{
+		const query = await db.query('SELECT collection_id, name FROM aperturama.collection WHERE owner_user_id = $1', [req.user.sub]);
+		res.json(query.rows);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // GET /collections/shared - Retrieve list of collections shared with user
 router.get('/shared', async(req, res) => {
 
-	const query = await db.query('SELECT aperturama.collection.collection_id, name FROM aperturama.collection JOIN aperturama.collection_sharing ON aperturama.collection.collection_id=aperturama.collection_sharing.collection_id WHERE shared_to_user_id = $1', [req.user.sub]);
-	// TODO: Error handling
-
-	res.json(query.rows);
+	try{
+		const query = await db.query('SELECT aperturama.collection.collection_id, name FROM aperturama.collection JOIN aperturama.collection_sharing ON aperturama.collection.collection_id=aperturama.collection_sharing.collection_id WHERE shared_to_user_id = $1', [req.user.sub]);
+		res.json(query.rows);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // POST /collections - Create a new collection
 router.post('/', async(req, res) => {
 
-	await db.query('INSERT INTO aperturama.collection (owner_user_id, name) VALUES ($1, $2)', [req.user.sub, req.body['name'] || 'Untitled']);
-	// TODO: Error handling
-
-	res.sendStatus(200);
+	try{
+		await db.query('INSERT INTO aperturama.collection (owner_user_id, name) VALUES ($1, $2)', [req.user.sub, req.body['name'] || 'Untitled']);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // GET /collections/<id> - Get a collection and its media
 router.get('/:id(\\d+)', auth_collection(), async(req, res) => {
 
-	let collection = {sharing: []};
+	try{
+		let collection = {sharing: []};
 
-	// Get collection metadata
-	let query = await db.query('SELECT name FROM aperturama.collection WHERE collection_id = $1', [req.params['id']]);
-	collection['name'] = query.rows[0]['name'];
-	// TODO: Error handling
+		// Get collection metadata
+		let query = await db.query('SELECT name FROM aperturama.collection WHERE collection_id = $1', [req.params['id']]);
+		collection['name'] = query.rows[0]['name'];
 
-	// Get users shared with
-	query = await db.query('SELECT email FROM aperturama.user JOIN aperturama.collection_sharing ON aperturama.user.user_id=aperturama.collection_sharing.shared_to_user_id WHERE aperturama.collection_sharing.collection_id = $1 AND aperturama.collection_sharing.shared_link_code IS NULL', [req.params['id']]);
-	if(query.rows.length > 0){
-		query.rows.forEach(row => collection['sharing'].push(row));
+		// Get users shared with
+		query = await db.query('SELECT email FROM aperturama.user JOIN aperturama.collection_sharing ON aperturama.user.user_id=aperturama.collection_sharing.shared_to_user_id WHERE aperturama.collection_sharing.collection_id = $1 AND aperturama.collection_sharing.shared_link_code IS NULL', [req.params['id']]);
+		if(query.rows.length > 0){
+			query.rows.forEach(row => collection['sharing'].push(row));
+		}
+
+		// Get links shared with
+		query = await db.query('SELECT shared_link_code AS code, shared_link_password AS password FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_to_user_id IS NULL', [req.params['id']]);
+		if(query.rows.length > 0){
+			query.rows.forEach(row => collection['sharing'].push(row));
+		}
+
+		// Get media in collection
+		query = await db.query('SELECT media_id FROM aperturama.collection_media WHERE collection_id = $1', [req.params['id']]);
+		collection['media'] = query.rows;
+
+		res.json(collection);
+	}catch(err){
+		res.sendStatus(500);
 	}
-	// TODO: Error handling
-
-	// Get links shared with
-	query = await db.query('SELECT shared_link_code AS code, shared_link_password AS password FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_to_user_id IS NULL', [req.params['id']]);
-	if(query.rows.length > 0){
-		query.rows.forEach(row => collection['sharing'].push(row));
-	}
-	// TODO: Error handling
-
-	// Get media in collection
-	query = await db.query('SELECT media_id FROM aperturama.collection_media WHERE collection_id = $1', [req.params['id']]);
-	collection['media'] = query.rows;
-	// TODO: Error handling
-
-	res.json(collection);
 
 });
 
 // PUT /collections/<id> - Update a collection (rename)
 router.put('/:id(\\d+)', auth_collection(), async(req, res) => {
 
-	// Update collection name
-	await db.query('UPDATE aperturama.collection SET name = $1 WHERE collection_id = $2', [req.body['name'], req.params['id']]);
-	// TODO: Error handling
+	try{
+		// Update collection name
+		await db.query('UPDATE aperturama.collection SET name = $1 WHERE collection_id = $2', [req.body['name'], req.params['id']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // DELETE /collections/<id> - Delete a collection
 router.delete('/:id(\\d+)', auth_collection(),async(req, res) => {
 
-	// Delete collection
-	await db.query('DELETE FROM aperturama.collection WHERE collection_id = $1', [req.params['id']]);
-	// TODO: Error handling
+	try{
+		// Delete collection
+		await db.query('DELETE FROM aperturama.collection WHERE collection_id = $1', [req.params['id']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
@@ -111,31 +123,36 @@ router.post('/:id(\\d+)', auth_collection(), auth_media(), async(req, res) => {
 // DELETE /collections/<collectionid>/media/<mediaid> - Remove media from collection
 router.delete('/:id(\\d+)/media/:mediaid(\\d+)', auth_collection(), async(req, res) => {
 
-	// Remove media from collection
-	await db.query('DELETE FROM aperturama.collection_media WHERE collection_id = $1 AND media_id = $2', [req.params['id'], req.params['mediaid']]);
-	// TODO: Error handling
+	try{
+		// Remove media from collection
+		await db.query('DELETE FROM aperturama.collection_media WHERE collection_id = $1 AND media_id = $2', [req.params['id'], req.params['mediaid']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // POST /collections/<id>/share/user - Share collection with a user
 router.post('/:id(\\d+)/share/user', auth_collection(), async(req, res) => {
 
-	// Get shared user's ID from email
-	const query = await db.query('SELECT user_id FROM aperturama.user WHERE email = $1', [req.body['email']]);
-	// TODO: Error handling
+	try{
+		// Get shared user's ID from email
+		const query = await db.query('SELECT user_id FROM aperturama.user WHERE email = $1', [req.body['email']]);
 
-	if(query.rows.length === 1){
+		if(query.rows.length === 1){
 
-		// Share collection with the user
-		await db.query('INSERT INTO aperturama.collection_sharing (collection_id, shared_to_user_id) VALUES ($1, $2)', [req.params['id'], query.rows[0]['user_id']]);
-		// TODO: Error handling
+			// Share collection with the user
+			await db.query('INSERT INTO aperturama.collection_sharing (collection_id, shared_to_user_id) VALUES ($1, $2)', [req.params['id'], query.rows[0]['user_id']]);
 
-		res.sendStatus(200);
+			res.sendStatus(200);
 
-	}else{
-		res.sendStatus(404);
+		}else{
+			res.sendStatus(404);
+		}
+	}catch(err){
+		res.sendStatus(500);
 	}
 
 });
@@ -143,48 +160,58 @@ router.post('/:id(\\d+)/share/user', auth_collection(), async(req, res) => {
 // DELETE /collections/<id>/share/user - Stop sharing collection with a user
 router.delete('/:id(\\d+)/share/user', auth_collection(), async(req, res) => {
 
-	await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_to_user_id = $2', [req.params['id'], req.body['user_id']]);
-	// TODO: Error handling
+	try{
+		await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_to_user_id = $2', [req.params['id'], req.body['user_id']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // POST /collections/<id>/share/link - Share collection with a link
 router.post('/:id(\\d+)/share/link', auth_collection(), async(req, res) => {
 
-	console.log(req.body);
+	try{
+		// Create random code for link if not given
+		const code = req.body['code'] ?? crypto.randomBytes(32).toString('base64url');
 
-	// Create random code for link if not given
-	const code = req.body['code'] ?? crypto.randomBytes(32).toString('base64url');
+		// Create link in database
+		await db.query('INSERT INTO aperturama.collection_sharing (collection_id, shared_link_code, shared_link_password) VALUES ($1, $2, $3)', [req.params['id'], code, req.body['password'] ?? null]);
 
-	// Create link in database
-	await db.query('INSERT INTO aperturama.collection_sharing (collection_id, shared_link_code, shared_link_password) VALUES ($1, $2, $3)', [req.params['id'], code, req.body['password'] ?? null]);
-	// TODO: Error handling
-
-	res.json({code: code});
+		res.json({code: code});
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // DELETE /collections/<id>/share/link/<code> - Stop sharing collection with a link
 router.delete('/:id(\\d+)/share/link/:code', auth_collection(), async(req, res) => {
 
-	// Delete shared link
-	await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_link_code = $2', [req.params['id'], req.params['code']]);
-	// TODO: Error handling (invalid code)
+	try{
+		// Delete shared link
+		await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1 AND shared_link_code = $2', [req.params['id'], req.params['code']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
 // DELETE /collections/<id>/share - Stop sharing collection with anyone
 router.delete('/:id(\\d+)/share', auth_collection(), async(req, res) => {
 
-	// Delete collection sharing entries
-	await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1', [req.params['id']]);
-	// TODO: Error handling
+	try{
+		// Delete collection sharing entries
+		await db.query('DELETE FROM aperturama.collection_sharing WHERE collection_id = $1', [req.params['id']]);
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	}catch(err){
+		res.sendStatus(500);
+	}
 
 });
 
